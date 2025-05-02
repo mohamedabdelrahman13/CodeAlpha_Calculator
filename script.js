462b19a5367ae57c09a16ec9fea7d22a1092850f
mohamedabdelrahman13 <mohamedrmcf897@gmail.com>
2025-05-02 22:58:40 +0300

    second Task Complete

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    display.value = eval(display.value);
+}
